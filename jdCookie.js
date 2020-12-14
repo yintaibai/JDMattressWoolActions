6a64b6a6243f3020d842d4bae5b550e49fe8c36b
yintaibai <74220634+yintaibai@users.noreply.github.com>
2020-12-14 12:52:23 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -3,7 +3,6 @@
  */
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
-  'pt_key=AAJf1un6ADBUUpRlbBQQH1QlV2tc1XRzDHcBiMyfG_0A-4I57GIEeitYQlCEd1bk1tBxdnyeOo4;pt_pin=yintaibai5087;',
 let CookieJDs = [
   'pt_key=AAJf1sD8ADDgBUrgWGJqYoxq_YLrcedhLW9QcB5UwtdMeMNnCoR6hhaRUYLLD9UkBEh10hohtYo;pt_pin=jd_UmKwrxDvcvUK;',
   'pt_key=AAJf1un6ADBUUpRlbBQQH1QlV2tc1XRzDHcBiMyfG_0A-4I57GIEeitYQlCEd1bk1tBxdnyeOo4;pt_pin=yintaibai5087;',
